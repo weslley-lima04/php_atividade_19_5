50c95eb65dac41f859d786b8032beb359c18e94d
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-05-19 19:25:12 -0300

    exercicio 3 ok

---
+++ exercicio3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+  <form method="POST" action="exercicio3_resp.php">
+  	<label>Digite o seu login:</label>
+  	<br><br>
+  	Usuário:
+  	<input type="text" name="user">
+  	<br><br>
+  	Senha:
+  	<input type="password" name="senha">
+  	<br><br>
+  	<input type="submit" name="submit">
+  </form>
+
+</body>
+</html>
