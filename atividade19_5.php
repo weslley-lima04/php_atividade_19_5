352013ca3a9b0ae1af6d6229466455dd390a4592
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-05-19 19:07:06 -0300

    exercicio1 ok

---
+++ atividade19_5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+  <form>
+  	<label>Digite o valor das áreas do retângulo</label>
+  	<br><br>
+  	Lado maior:
+  	<input type="number" name="num1">
+  	<br><br>
+  	Lado menor:
+  	<input type="number" name="num2">
+  </form>
+</body>
+</html>
